4736ce26a4e8f0b689c76910134f8a8cf74a307a
DESKTOP-5ABTQLN\yui-g <a8120042@gsuite.si.aoyama.ac.jp>
2022-05-10 18:35:57 +0900

    prac3-1

--- aandd.js
+++ aandd.js
@@ -1,15 +1,34 @@
-function show(array) {
-    var html = '';
-    html = html + '<table border="1">';
-    html = html + '<tr>';
-    for (var i = 0; i < array.length; i++) {
-        html = html + '<td>' + array[i] + '</td>';
+function show(obj) {
+    if (Array.isArray(obj)) {
+        var html = '';
+        html = html + '<table border="1">';
+        html = html + '<tr>';
+        for (var i = 0; i < obj.length; i++) {
+            html = html + '<td>' + obj[i] + '</td>';
+        }
+        html = html + '</tr>';
+        html = html + '</table>';
+        //html = html + '<br>';
+        document.getElementById('res').insertAdjacentHTML('beforeend', html);
+    } else {
+        var string = JSON.stringify(obj);
+        string = string + '<br>';
+        document.getElementById('res').insertAdjacentHTML('beforeend', string);
     }
-    html = html + '</tr>';
-    html = html + '</table>';
-    //document.getElementById('res').innerHTML = html;
-    document.getElementById('res').insertAdjacentHTML('beforeend', html);
 }
+
+// function show(array) {
+//     var html = '';
+//     html = html + '<table border="1">';
+//     html = html + '<tr>';
+//     for (var i = 0; i < array.length; i++) {
+//         html = html + '<td>' + array[i] + '</td>';
+//     }
+//     html = html + '</tr>';
+//     html = html + '</table>';
+//     //document.getElementById('res').innerHTML = html;
+//     document.getElementById('res').insertAdjacentHTML('beforeend', html);
+// }
 
 function createSequentialNumbers(array, n) {
     for (var i = 1; i < n + 1; i++) {
